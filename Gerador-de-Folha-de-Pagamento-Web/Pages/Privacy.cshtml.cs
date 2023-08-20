fa6e1d0af347a325a1ecdf1e3836d4eb8da410c0
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-08-20 20:51:43 -0300

    Adicionar arquivos de projeto.

---
+++ Gerador-de-Folha-de-Pagamento-Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gerador_de_Folha_de_Pagamento_Web.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
