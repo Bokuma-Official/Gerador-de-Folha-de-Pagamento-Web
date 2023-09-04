430869aaf5c5eb353e274a8e801a321badba2595
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-09-03 21:16:24 -0300

    Início do projeto

---
+++ Gerador-de-Folha-de-Pagamento-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerador_de_Folha_de_Pagamento_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
