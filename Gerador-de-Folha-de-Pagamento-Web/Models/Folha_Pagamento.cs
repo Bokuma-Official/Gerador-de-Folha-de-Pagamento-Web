b9f381b5b945c450198cb4956cdb82ada88bed6d
Marcos Antonio <eusoumarcos1331@gmail.com>
2023-10-14 16:02:33 -0300

    Realizado a Integração do Banco de dados com a Aplicação WEB, necessario testes de funcionamento e inicio do desenvolvimento das paginas de folha de Pagamento e Informações do Funcionario

---
+++ Gerador-de-Folha-de-Pagamento-Web/Models/Folha_Pagamento.cs
@@ -0,0 +1,28 @@
+﻿namespace Gerador_de_Folha_de_Pagamento_Web.Models
+{
+    public class Folha_Pagamento
+    {
+        public int ID_Folha_Pagamento { get; set; }
+        public string Data_Pagamento { get; set; }
+        public int Horas_Trabalhadas { get; set; }
+        public string Valor_Hora { get; set; }
+        public int Horas_Faltas { get; set; }
+        public string Desconto_Horas_Faltas { get; set; }
+        public int Horas_Extras { get; set; }
+        public string Valor_Horas_Extras { get; set; }
+        public string Valor_Vale_Transporte { get; set; }
+        public string Valor_Vale_Alimentacao { get; set; }
+        public string Desconto_INSS { get; set; }
+        public string Desconto_FGTS { get; set; }
+        public string Desconto_IRRF { get; set; }
+        public string Desconto_Vale_Transporte { get; set; }
+        public string Desconto_Vale_Alimentacao { get; set; }
+        public string Desconto_Seguro_Vida { get; set; }
+        public int Dias_Ferias { get; set; }
+        public string Valor_Ferias { get; set; }
+        public string Valor_13_Salario { get; set; }
+        public string Salario_Bruto { get; set; }
+        public string Salario_Liquido { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ Gerador-de-Folha-de-Pagamento-Web/Models/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿namespace Gerador_de_Folha_de_Pagamento_Web.Models
+{
+    public class Funcionario
+    {
+        public string CPF { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Data_Nascimento { get; set; }
+        public string Sexo { get; set; }
+        public string PCD { get; set; }
+        public string PIS { get; set; }
+        public string RG { get; set; }
+        public string Carteira_Trabalho { get; set; }
+        public string Titulo_Eleitor { get; set; }
+        public string Certificado_Militar { get; set; }
+        public int Matricula { get; set; }
+        public string Telefone_Fixo { get; set; }
+        public string Telefone_Celular { get; set; }
+        public string Email { get; set; }
+        public int Dependentes { get; set; }
+    }
+}
