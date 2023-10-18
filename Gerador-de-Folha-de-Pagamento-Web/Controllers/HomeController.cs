b03d6e5f4674ddc84e471641e6403e622a75242c
Matheus Cordeiro <71728308+Matheus-C5@users.noreply.github.com>
2023-10-18 17:51:23 -0300

    Visualizar Folhas de Pagamento de acordo com o Funcionário que fez o login funcionando. Também foi adicionado comentários.

--- Gerador-de-Folha-de-Pagamento-Web/Controllers/HomeController.cs
+++ Gerador-de-Folha-de-Pagamento-Web/Controllers/HomeController.cs
@@ -13,6 +13,9 @@ namespace Gerador_de_Folha_de_Pagamento_Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly Banco_Folha_Pagamento_Ataron_Contexto _contexto;
+        public static string CPF_Funcionario_Logado { get; set; }
+        public static string Nome_Funcionario_Logado { get; set; }
+        public static string Cargo_Funcionario_Logado { get; set; }
 
         public HomeController(ILogger<HomeController> logger, Banco_Folha_Pagamento_Ataron_Contexto contexto)
         {
@@ -20,39 +23,74 @@ namespace Gerador_de_Folha_de_Pagamento_Web.Controllers
             _contexto = contexto;
         }
 
+        // método para zerar as variáveis estáticas
+        public void Resetar_Variaveis()
+        {
+            CPF_Funcionario_Logado = null;
+            Nome_Funcionario_Logado = null;
+            Cargo_Funcionario_Logado = null;
+        }
+
+        // quando retornar a tela de login, zera as variáveis estáticas
         [HttpGet]
         public IActionResult index()
         {
+            Resetar_Variaveis();
             Funcionario_Login _funcionario_login = new Funcionario_Login();
             return View(_funcionario_login);
         }
 
+        /* verificar se existe um cpf e senha iguais no banco de dados ao que foi digitado na tela, 
+        também vai salvar o nome e o cargo do funcionário para mostrar na tela de menu se o login for bem sucedido */
         [HttpPost]
         public IActionResult index(Funcionario_Login _funcionario_login)
         {
-            var login = _contexto.Funcionario.Where(funcionario => funcionario.CPF == _funcionario_login.CPF && funcionario.Senha == _funcionario_login.Senha).FirstOrDefault();
+            var login_funcionario = _contexto.Funcionario.Where(funcionario => funcionario.CPF == _funcionario_login.CPF && funcionario.Senha == _funcionario_login.Senha).FirstOrDefault();
 
-            if (login == null)
+            if (login_funcionario == null)
             {
-                ViewBag.LoginStatus = 0;
+                ViewBag.login_funcionario = 0;
             }
 
             else
             {
+                CPF_Funcionario_Logado = _funcionario_login.CPF;
+                Nome_Funcionario_Logado = login_funcionario.Nome;
+
+                var contrato_funcionario = _contexto.Contrato_Empresa.FirstOrDefault(contrato_empresa => contrato_empresa.CPF == CPF_Funcionario_Logado);
+
+                if (contrato_funcionario != null)
+                {
+                    Cargo_Funcionario_Logado = contrato_funcionario.Cargo;
+                }
+
                 return RedirectToAction("Menu", "Home");
             }
 
             return View(_funcionario_login);
         }
 
+        // exibir o nome e o cargo do funcionário
         public IActionResult Menu()
         {
+            ViewBag.nome_funcionario_logado = Nome_Funcionario_Logado;
+            ViewBag.cargo_funcionario_logado = Cargo_Funcionario_Logado;
             return View();
         }
 
+        // se clicar no botão de sair da conta, zera as variáveis estáticas e retorna a tela inicial
+        public IActionResult Voltar_Inicio()
+        {
+            Resetar_Variaveis();
+            return RedirectToAction("index", "Home");
+        }
+
+        // exibir as folhas de pagamento de acordo com o funcionário que fez o login
         public IActionResult Visualizar_Folhas_Pagamento()
         {
-            return View();
+            var folhas_de_pagamento_funcionario = _contexto.Folha_Pagamento.Where(folha_pagamento => folha_pagamento.CPF == CPF_Funcionario_Logado).ToList();
+
+            return View(folhas_de_pagamento_funcionario);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
